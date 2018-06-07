39e47139d8fd2c30d5cc6190c64fd7f71315ea9a
wjl <wjl7123093@163.com>
2018-06-07 10:41:27 +0800

    修改了 签到列表 页面

--- app/src/main/java/com/mypolice/poo/ui/activity/SignListActivity.java
+++ app/src/main/java/com/mypolice/poo/ui/activity/SignListActivity.java
@@ -8,6 +8,7 @@ import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.mypolice.poo.R;
 import com.mypolice.poo.adapter.CommonAdapter;
 import com.mypolice.poo.adapter.ViewHolder;
+import com.mypolice.poo.application.ApiCode;
 import com.mypolice.poo.application.GlobalSet;
 import com.mypolice.poo.bean.WorkBean;
 import com.mypolice.poo.service.KeepLiveService;
@@ -19,11 +20,16 @@ import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,6 +38,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
 import okhttp3.Call;
 
 /**   
@@ -43,7 +50,7 @@ import okhttp3.Call;
  * @update 2017-11-14
  * @version v2.1.2(14)
  */
-@ContentView(R.layout.activity_sign_list)
+@ContentView(R.layout.activity_sign_list_new)
 public class SignListActivity extends BaseActivityPoo {
 
 	private static final int REQUES_CODE_SIGN = 0x301;
@@ -58,8 +65,26 @@ public class SignListActivity extends BaseActivityPoo {
 	private MyListView mLvSignPre;
 	@ViewInject(R.id.llExpandSwitch)
 	private LinearLayout mLlExpandSwitch;
-	@ViewInject(R.id.ivSwitch)
-	private ImageView mIvSwitch;
+//	@ViewInject(R.id.ivSwitch)
+//	private ImageView mIvSwitch;
+
+	@ViewInject(R.id.ll_sign_btn)
+	private LinearLayout mLlSignBtn;
+	@ViewInject(R.id.ll_sign_pre_notice_title)
+	private LinearLayout mLlSignPreWorkTitle;
+	@ViewInject(R.id.ll_sign_pre_notice_nowork)
+	private LinearLayout mLlSignPreWorkNowork;
+
+	@ViewInject(R.id.tv_sign_time)
+	private TextView mTvSignTime;
+	@ViewInject(R.id.tv_sign_type)
+	private TextView mTvSignType;
+	@ViewInject(R.id.btn_sign)
+	private Button mBtnSign;
+	@ViewInject(R.id.tv_count)
+	private TextView mTvCounts;
+
+	private int mWorkId = 0;	// 任务ID
 
 	private CommonAdapter mAdapter;
 	private CommonAdapter mAdapterPre;
@@ -67,7 +92,7 @@ public class SignListActivity extends BaseActivityPoo {
 	private boolean isExpand = false;	// 是否展开[默认关闭]
 
 	/** 加载进度条 */
-	private CenterDialog centerDialog;
+	private SweetAlertDialog pDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +100,6 @@ public class SignListActivity extends BaseActivityPoo {
 		ViewUtils.inject(this);
 
 		initView();
-//		getCommunityWork();
 		getSignList();
 	}
 	
@@ -83,12 +107,12 @@ public class SignListActivity extends BaseActivityPoo {
 	public void initView() {
 		super.initView();
 		mTitleSignList.setText("定期签到");
-		mLlExpandSwitch.setVisibility(View.GONE);
-		mLvSignPre.setVisibility(View.GONE);
 
-		centerDialog = new CenterDialog(SignListActivity.this, R.layout.dialog_wap_loading,
-				new int[]{});
-		centerDialog.show();
+		pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+		pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+		pDialog.setTitleText("正在加载...");
+		pDialog.setCancelable(false);
+		pDialog.show();
 	}
 
 	@Override
@@ -101,117 +125,67 @@ public class SignListActivity extends BaseActivityPoo {
 		}
 	}
 
-	/** 展开/合并开关 点击事件 */
-	@OnClick(R.id.llExpandSwitch)
-	public void onViewExpandSwitchClick(View v) {
-		if (isExpand) {
-			isExpand = false;
-			mIvSwitch.setImageResource(R.mipmap.ic_arrow_down);
-			mLvSignPre.setVisibility(View.GONE);
-		} else {
-			isExpand = true;
-			mIvSwitch.setImageResource(R.mipmap.ic_arrow_up);
-			mLvSignPre.setVisibility(View.VISIBLE);
-		}
-	}
-
-	/**
-	 * 获取 特定吸毒者所有签到记录
-	 */
-	private void getCommunityWork() {
-		String url = GlobalSet.APP_SERVER_URL + "community_work/conditionGet";
-		OkHttpUtils.post().url(url)
-				.addHeader("token", mApplication.getToken())
-				.addParams("conditions", "(work_tag=0 or work_tag=1) and (work_type=3) and drug_user_id=" + mApplication.getUserID())
-				.addParams("fileds", "id,work_type,work_time,remark,work_tag")
-//				.addParams("id", mApplication.getUserID() + "")
-				.build()
-				.execute(new StringCallback() {
-					@Override
-					public void onError(Call call, Exception e, int id) {
-						CommonFuncUtil.getToast(SignListActivity.this, e.getMessage());
-						centerDialog.cancel();
-					}
-
-					@Override
-					public void onResponse(String response, int id) {
-//						CommonFuncUtil.getToast(PhysicalExaminationActivity.this, response);
-						centerDialog.cancel();
-						try {
-							JSONObject jsonResponse = new JSONObject(response);
-							if (jsonResponse.getInt("code") == 0
-									|| jsonResponse.getInt("code") == 200) {
-								org.json.JSONArray array = jsonResponse.getJSONObject("data").getJSONArray("data");
-								List<WorkBean> workBeanList = new ArrayList<WorkBean>();
-								WorkBean work = null;
-								for (int i = 0; i < array.length(); i++) {
-									work = JSON.parseObject(array.getString(i), WorkBean.class);
-//									CommonFuncUtil.getToast(SignListActivity.this, work.toString());
-									workBeanList.add(work);
-								}
-								bindDataToUI(workBeanList);
-							} else if (jsonResponse.getInt("code") == 1007) {
-								// token 失效，踢出当前用户，退到登录页面
-								CommonFuncUtil.getToast(SignListActivity.this,
-										"当前用户已在别处登录，请重新登录");
-								removeALLActivity();
-								CommonFuncUtil.goNextActivityWithNoArgs(SignListActivity.this,
-										LoginActivity.class, false);
-							}
-						} catch (JSONException e) {
-							e.printStackTrace();
-						}
-					}
-				});
+	/** 跳转到签到页面 */
+	@OnClick(R.id.btn_sign)
+	public void onBtnSignClick(View v) {
+		Bundle bundle = new Bundle();
+		bundle.putInt("taskId", mWorkId);
+		CommonFuncUtil.goNextActivityWithArgsForResult(SignListActivity.this,
+				SignActivity.class, bundle, REQUES_CODE_SIGN);
 	}
 
 	/**
 	 * 获取签到列表 [New]
 	 */
 	private void getSignList() {
-		String url = GlobalSet.APP_SERVER_URL + "community_work/getWorkListBy";
+		String url = GlobalSet.APP_SERVER_URL + "app.sign/mySign";
 		OkHttpUtils.post().url(url)
-				.addHeader("token", mApplication.getToken())
-				.addParams("drug_user_id", mApplication.getUserID() + "")
-				.addParams("work_type", "3")	// 3 -> 签到
+				.addHeader(GlobalSet.APP_TOKEN_KEY, mApplication.getToken())
 				.build()
 				.execute(new StringCallback() {
 					@Override
 					public void onError(Call call, Exception e, int id) {
+						pDialog.dismiss();
 						CommonFuncUtil.getToast(SignListActivity.this, e.getMessage());
 					}
 
 					@Override
 					public void onResponse(String response, int id) {
-//						CommonFuncUtil.getToast(SignActivity.this, response);
-						mLlExpandSwitch.setVisibility(View.VISIBLE);
-						centerDialog.cancel();
+						pDialog.dismiss();
 						try {
 							JSONObject jsonResponse = new JSONObject(response);
-							if (jsonResponse.getInt("code") == 0
-									|| jsonResponse.getInt("code") == 200) {
-								JSONArray arrNow = jsonResponse.getJSONObject("data").getJSONArray("nowList");
-								JSONArray arrPre = jsonResponse.getJSONObject("data").getJSONArray("preList");
+							if (jsonResponse.getInt("code") == ApiCode.CODE_SUCCESS) {
+								JSONArray arrWorkLog = jsonResponse.getJSONObject("data").getJSONArray("work_log");
+								String work = jsonResponse.getJSONObject("data").getString("work");
+								String workNum = jsonResponse.getJSONObject("data").getString("work_num");
 
-								List<WorkBean> workBeanList = new ArrayList<WorkBean>();
-								WorkBean work = null;
-								for (int i = 0; i < arrNow.length(); i++) {
-									work = JSON.parseObject(arrNow.getString(i), WorkBean.class);
-									workBeanList.add(work);
+								if (!TextUtils.isEmpty(work) && !work.equals("{}")
+										&& !work.equals("null")) {	// 当前任务
+									WorkBean workBean = JSON.parseObject(work, WorkBean.class);
+									bindWorkDataToUI(workBean);
+//									// 显示提示区
+//									setUranNoticeBlockVisibility(View.VISIBLE);
+								} else {
+									// 隐藏尿检操作区
+//									setUranNoticeBlockGone();
+//									setNoTaskVisible();
 								}
-								List<WorkBean> workBeanPreList = new ArrayList<WorkBean>();
-								WorkBean workPre = null;
-								for (int i = 0; i < arrPre.length(); i++) {
-									workPre = JSON.parseObject(arrPre.getString(i), WorkBean.class);
-									workBeanPreList.add(workPre);
+								if (!TextUtils.isEmpty(workNum) && !workNum.equals("{}")
+										&& !workNum.equals("null")) {	// 任务数量
+									WorkBean workNumBean = JSON.parseObject(workNum, WorkBean.class);
+									bindWorkNumDataToUI(workNumBean);
 								}
-
-//								if (workBeanList.size() > 0)
-									bindDataToUI(workBeanList);
-//								if (workBeanPreList.size() > 0)
+								if (arrWorkLog.length() > 0) {	// 家访记录
+									List<WorkBean> workBeanPreList = new ArrayList<WorkBean>();
+									WorkBean workPre = null;
+									for (int i = 0; i < arrWorkLog.length(); i++) {
+										workPre = JSON.parseObject(arrWorkLog.getString(i), WorkBean.class);
+										workBeanPreList.add(workPre);
+									}
 									bindDataToUIPre(workBeanPreList);
+								}
 
-							} else if (jsonResponse.getInt("code") == 1007) {
+							} else if (jsonResponse.getInt("code") == ApiCode.CODE_TOKEN_EXPIRED) {
 								// token 失效，踢出当前用户，退到登录页面
 								CommonFuncUtil.getToast(SignListActivity.this,
 										"当前用户已在别处登录，请重新登录");
@@ -241,9 +215,20 @@ public class SignListActivity extends BaseActivityPoo {
 						+ item.getRemark());
 //				helper.setText(R.id.tvItemStatus, GlobalSet.WORK_STATUS_HEAD
 //						+ (item.getWork_tag() == 0 ? "未登记" : "已登记(待审核)"));
-				helper.setText(R.id.tvItemStatus, GlobalSet.WORK_STATUS_HEAD
-						+ item.getWork_tag_text());
-				helper.setText(R.id.btnItemUpdateWork, item.getWork_tag() == 0 ? "登记" : "修改");
+//				helper.setText(R.id.btnItemUpdateWork, item.getWork_tag() == 0 ? "登记" : "修改");
+
+				switch (item.getWork_tag()) {
+					case 0:
+						helper.setText(R.id.tvItemStatus, "新任务");
+						break;
+					case 1:
+						helper.setText(R.id.tvItemStatus, "待审核");
+						break;
+					case 2:
+						helper.setText(R.id.tvItemStatus, "已完成");
+						break;
+				}
+
 				helper.setOnClickListener(R.id.btnItemUpdateWork, new View.OnClickListener() {
 					@Override
 					public void onClick(View v) {
@@ -267,26 +252,90 @@ public class SignListActivity extends BaseActivityPoo {
 				R.layout.item_lv_pre_work) {
 			@Override
 			public void convert(ViewHolder helper, final WorkBean item) {
-				helper.setText(R.id.tvItemDeadtime, GlobalSet.WORK_TIME_HEAD
-						+ item.getWork_time());
-				helper.setText(R.id.tvItemDescription, GlobalSet.WORK_DESCRIPTION_HEAD
-						+ item.getRemark());
-				helper.setIcon(R.id.iconItemStatus,
-						item.getWork_tag() == 2 ? getString(R.string.icon_sign_ok)
-								: getString(R.string.icon_sign_err),
-						item.getWork_tag() == 2 ? getResources().getColor(R.color.app_main_green)
-								: getResources().getColor(R.color.app_main_red));
+				if (item == getItem(0)) {
+					helper.getView(R.id.view_1).setVisibility(View.INVISIBLE);
+				}
+
+				/*helper.setText(R.id.tvItemDeadtime, GlobalSet.WORK_TIME_HEAD + item.getWork_time());
+				helper.setText(R.id.tvItemDescription, GlobalSet.WORK_DESCRIPTION_HEAD + item.getRemark());*/
+				((TextView) helper.getView(R.id.tvItemDeadtime)).setText(Html.fromHtml(GlobalSet.WORK_TIME_HEAD
+						+ "<font color=\"#999999\">" + item.getWork_time() + "</font>"));
+				((TextView) helper.getView(R.id.tvItemDescription)).setText(Html.fromHtml(GlobalSet.WORK_DESCRIPTION_HEAD
+						+ "<font color=\"#999999\">" + item.getRemark() + "</font>"));
+
+				String status = "";
+				int resId = R.mipmap.ic_warning;
+				switch (item.getWork_tag()) {
+					case 0:
+						status = "<font color=\"#ffa400\">新任务</font>";
+						resId = R.mipmap.ic_warning;
+						break;
+					case 1:
+						status = "<font color=\"#ffa400\">待审核</font>";
+						resId = R.mipmap.ic_warning;
+						break;
+					case 2:
+						status = "<font color=\"#53d656\">已完成</font>";
+						resId = R.mipmap.ic_ok;
+						break;
+					case 3:
+						status = "<font color=\"#f14f4f\">未通过</font>";
+						resId = R.mipmap.ic_error;
+						break;
+				}
+				helper.setImageResource(R.id.iconItemStatus, resId);
+				((TextView) helper.getView(R.id.tvItemStatus)).setText(Html.fromHtml("任务状态: " + status));
+//				helper.setText(R.id.tvItemTimes, item.getThe_first_year() + "第" + item.getNum() + "次");
+				helper.setVisibility(R.id.tvItemTimes, View.GONE);
 
 			}
 		};
 		mLvSignPre.setAdapter(mAdapterPre);
 	}
 
+	/** 绑定当前尿检数据 */
+	private void bindWorkDataToUI(WorkBean workBean) {
+		mWorkId = workBean.getWork_id();
+//		mTvNotice.setText("亲～" + workBean.getWork_time() + "要完成本期签到检任务哦～～");
+		mTvSignTime.setText("签到时间  " + workBean.getWork_time());
+		mTvSignType.setText("任务说明  " + workBean.getRemark());
+
+		List<WorkBean> workList = new ArrayList<>();
+		workList.add(workBean);
+		switch (workBean.getWork_tag()) {
+			case 0: // 新任务
+				mLvSign.setVisibility(View.GONE);
+				break;
+			case 1:	// 待审核
+			case 2: // 已完成
+				bindDataToUI(workList);
+				setUranNoticeBlockGone();
+				break;
+		}
+	}
+
+	/** 绑定任务数量数据 */
+	private void bindWorkNumDataToUI(WorkBean workBean) {
+		mTvCounts.setText("已完成:" + workBean.getFinish() + "次  |  共需完成:" + workBean.getSum() + "次");
+	}
+
+	/** 隐藏 任务提示区 */
+	private void setUranNoticeBlockGone() {
+//		mLlSignNotice.setVisibility(View.GONE);
+		mLlSignBtn.setVisibility(View.GONE);
+	}
+
+	/** 显示 无任务区 */
+	private void setNoTaskVisible() {
+		mLlSignPreWorkTitle.setVisibility(View.VISIBLE);
+		mLlSignPreWorkNowork.setVisibility(View.VISIBLE);
+	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (requestCode == REQUES_CODE_SIGN && resultCode == SignActivity.RESULT_CODE_SIGN) {
-			centerDialog.show();
+			pDialog.show();
 			// 刷新列表
 //			getCommunityWork();
 			getSignList();
